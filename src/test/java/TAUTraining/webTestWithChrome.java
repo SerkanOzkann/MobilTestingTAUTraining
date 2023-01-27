2e27361a69161f3a792b88bda9dcc50a4d09f797
Furkan <furkan.ovut@virgosol.com>
2023-01-27 17:51:38 +0300

    sondurum

--- src/test/java/TAUTraining/webTestWithChrome.java
+++ src/test/java/TAUTraining/webTestWithChrome.java
@@ -2,6 +2,7 @@ package TAUTraining;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
@@ -17,8 +18,15 @@ public class webTestWithChrome {
     public AppiumDriver driver;
 
     By webBrowser = By.id("search_box_text");
+    By urlBar = By.id("url_bar");
 
+    By siteLogin = By.id("username");
 
+    By sitePassword = By.id("password");
+
+    By loginButton = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View[5]/android.view.View[3]/android.widget.Button");
+
+    By web = By.xpath("//android.widget.FrameLayout[@content-desc=\"Web Görüntüleme\"]");
 
         @BeforeTest
         public void beforeTest(){
@@ -45,10 +53,23 @@ public class webTestWithChrome {
         }
         @Test
         public void test() throws InterruptedException {
-             driver.findElement(webBrowser).click();
-             Thread.sleep(3000);
-
-
+            driver.findElement(webBrowser).click();
+            Thread.sleep(3000);
+            WebElement urlBarSearch = driver.findElement(urlBar);
+            urlBarSearch.sendKeys("https://the-internet.herokuapp.com/login");
+            Thread.sleep(3000);
+            driver.getKeyboard().pressKey(Keys.ENTER);
+            Thread.sleep(9000);
+            driver.findElement(web).click();
+            Thread.sleep(3000);
+           /* driver.findElement(siteLogin).click();
+            driver.findElement(siteLogin).sendKeys("tomsmith");*/
+            Thread.sleep(3000);
+            driver.findElement(sitePassword).click();
+            driver.findElement(sitePassword).sendKeys("SuperSecretPassword!");
+            Thread.sleep(3000);
+            driver.findElement(loginButton).click();
+            Thread.sleep(9000);
 
 
 
